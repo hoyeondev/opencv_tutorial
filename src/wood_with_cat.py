99d221c3bb65839acfe3cc5729052273ed237846
Hoyeon Park <133315262+hoyeondev@users.noreply.github.com>
2025-07-29 12:20:42 +0900

    cat3 추가, y 영역 수정

--- src/wood_with_cat.py
+++ src/wood_with_cat.py
@@ -5,6 +5,7 @@ import random
 #--- 이미지 불러오기
 cat1 = cv2.imread('../img/cat1.jpg')
 cat2 = cv2.imread('../img/cat2.jpg')
+cat3 = cv2.imread('../img/cat3.jpg')
 bg = cv2.imread('../img/forest.jpg')
 
 #--- 배경 크기 조정
@@ -37,12 +38,12 @@ def chroma_add(bg, fg, x, y):
     bg[y:y+h, x:x+w] = combined
 
 #--- 메인 처리: 랜덤으로 고양이 추가
-for _ in range(10):
+for _ in range(5):
     # 랜덤으로 cat1 또는 cat2 선택
-    cat_img = random.choice([cat1, cat2])
+    cat_img = random.choice([cat1, cat2, cat3])
 
     # 크기 조정: cat1은 300픽셀, cat2는 100픽셀로 조정
-    if cat_img is cat1:
+    if cat_img is cat1 or cat_img is cat3:
         new_width = 300  # cat1은 300픽셀
     else:
         new_width = 100
@@ -53,9 +54,14 @@ for _ in range(10):
 
     # 랜덤 위치 계산 (ROI가 배경 안에 들어가도록)
     max_x = bg.shape[1] - new_width
-    max_y = bg.shape[0] - new_height
+    #max_y = bg.shape[0] - new_height
+    
     x = random.randint(0, max_x)
-    y = random.randint(0, max_y)
+    #y = random.randint(0, max_y)
+
+    y_min = bg.shape[0] - int(bg.shape[0] * 0.4)
+    y_max = bg.shape[0] - new_height
+    y = random.randint(y_min, y_max)
 
     # 합성
     chroma_add(bg, resized_cat, x, y)
