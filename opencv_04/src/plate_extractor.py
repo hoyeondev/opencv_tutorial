314a2de56eda96ca2bb0662f7d4fea63e3e12eeb
Hoyeon Park <133315262+hoyeondev@users.noreply.github.com>
2025-07-31 11:50:20 +0900

    윈도우 명 변경, 변수명 주석추가

--- opencv_04/src/plate_extractor.py
+++ opencv_04/src/plate_extractor.py
@@ -3,16 +3,17 @@
 import cv2
 import numpy as np
 
-win_name = "scanning"
-img = cv2.imread("../img/paper.jpg")
+win_name = "License Plate Extractor"
+img = cv2.imread("../img/car_02.jpg")
 rows, cols = img.shape[:2]
-draw = img.copy()
-pts_cnt = 0
-pts = np.zeros((4,2), dtype=np.float32)
+draw = img.copy() # 점을 그릴 이미지 복사본
+pts_cnt = 0 # 클릭한 점의 개수
+pts = np.zeros((4,2), dtype=np.float32) # 클릭한 점의 좌표를 저장할 배열
+
 
 def onMouse(event, x, y, flags, param):  #마우스 이벤트 콜백 함수 구현 ---① 
     global  pts_cnt                     # 마우스로 찍은 좌표의 갯수 저장
-    if event == cv2.EVENT_LBUTTONDOWN:  
+    if event == cv2.EVENT_LBUTTONDOWN:  # 좌클릭 이벤트
         cv2.circle(draw, (x,y), 10, (0,255,0), -1) # 좌표에 초록색 동그라미 표시
         cv2.imshow(win_name, draw)
 
