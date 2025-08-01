1a4941944f5aeb3393cac3a5704035bea533f597
Hoyeon Park <133315262+hoyeondev@users.noreply.github.com>
2025-08-01 10:14:01 +0900

    qr 이미지 show

--- opencv_05/src/qr_scan.py
+++ opencv_05/src/qr_scan.py
@@ -2,7 +2,9 @@ import cv2
 import matplotlib.pyplot as plt
 import pyzbar.pyzbar as pyzbar
 
-cv2.imread('../img/frame.png')
+img = cv2.imread('../img/frame.png')
+plt.imshow(img)
+plt.show()
 
 cv2.waitKey(0)
 cv2.destroyAllWindows()
