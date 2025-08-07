64adc5ce3bba5d7857c0d04c24b6838f4953752f
Hoyeon Park <133315262+hoyeondev@users.noreply.github.com>
2025-08-07 17:34:26 +0900

    메인 : ear 함수 추가, 눈 인덱스 변수 추가

--- opencv_09/drowsiness_detection/main.py
+++ opencv_09/drowsiness_detection/main.py
@@ -1,6 +1,11 @@
 import cv2
 import dlib
 from basic_landmark import detect_faces
+import utils
+
+# 눈 인덱스
+LEFT_EYE_IDX = [36, 37, 38, 39, 40, 41]
+RIGHT_EYE_IDX = [42, 43, 44, 45, 46, 47]
 
 
 def main():
@@ -8,7 +13,6 @@ def main():
     # 초기화
 
     detector = dlib.get_frontal_face_detector()
-
     predictor = dlib.shape_predictor('../src/shape_predictor_68_face_landmarks.dat')
 
     cap = cv2.VideoCapture(0)
@@ -23,10 +27,10 @@ def main():
 
         frame = cv2.flip(frame, 1) # 카메라 좌우반전
 
-        # 1단계: 얼굴 검출
+        # 1단계: 얼굴 검출 및 랜드마크 추출
 
         # faces = detect_faces(frame, detector)
-        faces, result_img = detect_faces(frame, detector, predictor, draw_landmarks=True)
+        faces, landmarks, result_img = detect_faces(frame, detector, predictor, draw_landmarks=True)
 
         cv2.imshow('Drowsiness Detection', result_img)
         
@@ -41,9 +45,14 @@ def main():
 
             
 
-        #     # 3단계: EAR 계산
+            # 3단계: EAR 계산
+            # print(landmarks)
+            # ear_value = calculate_ear_from_landmarks(landmarks)
+            left_ear = utils.calculate_ear_from_landmarks(landmarks, LEFT_EYE_IDX)
+            right_ear = utils.calculate_ear_from_landmarks(landmarks, RIGHT_EYE_IDX)
+            avg_ear = (left_ear + right_ear) / 2.0
 
-        #     ear_value = calculate_ear_from_landmarks(landmarks)
+            print(avg_ear)
 
             
 
