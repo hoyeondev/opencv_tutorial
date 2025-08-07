67c1bde6d3d55a32d2dffd53a08322c911e8542d
Hoyeon Park <133315262+hoyeondev@users.noreply.github.com>
2025-08-07 17:43:32 +0900

    메인 : 졸음 판단 및 결과 표시

--- opencv_09/drowsiness_detection/main.py
+++ opencv_09/drowsiness_detection/main.py
@@ -7,15 +7,17 @@ import utils
 LEFT_EYE_IDX = [36, 37, 38, 39, 40, 41]
 RIGHT_EYE_IDX = [42, 43, 44, 45, 46, 47]
 
+EAR_THRESHOLD = 0.25
+CLOSED_EYES_FRAME_THRESHOLD = 150  # 5초 기준 (30fps 가정)
 
 def main():
 
     # 초기화
-
     detector = dlib.get_frontal_face_detector()
     predictor = dlib.shape_predictor('../src/shape_predictor_68_face_landmarks.dat')
-
     cap = cv2.VideoCapture(0)
+    closed_eyes_frame_count = 0
+    drowsy = False
 
     while cap.isOpened():
 
@@ -30,31 +32,43 @@ def main():
         # 1단계: 얼굴 검출 및 랜드마크 추출
 
         # faces = detect_faces(frame, detector)
-        faces, landmarks, result_img = detect_faces(frame, detector, predictor, draw_landmarks=True)
+        faces, landmarks, frame = detect_faces(frame, detector, predictor, draw_landmarks=True)
 
-        cv2.imshow('Drowsiness Detection', result_img)
+        # 안내 메세지 출력
+        cv2.putText(frame, "ESC: Exit", (10, 30), 
+                cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 255, 0), 2, cv2.LINE_AA)
+        cv2.imshow('Drowsiness Detection', frame)
         
 
         if len(faces) > 0:
 
-            print(len(faces))
+            # print(len(faces))
 
-        #     # 2단계: 랜드마크 추출
-
-        #     landmarks = get_landmarks(frame, faces[0], predictor)
-
-            
-
-            # 3단계: EAR 계산
+            # 2단계: EAR 계산
             # print(landmarks)
             # ear_value = calculate_ear_from_landmarks(landmarks)
             left_ear = utils.calculate_ear_from_landmarks(landmarks, LEFT_EYE_IDX)
             right_ear = utils.calculate_ear_from_landmarks(landmarks, RIGHT_EYE_IDX)
             avg_ear = (left_ear + right_ear) / 2.0
 
-            print(avg_ear)
+            # print(avg_ear)
 
+            # 3단계: 졸음 판단
+
+            if avg_ear < EAR_THRESHOLD:
+                closed_eyes_frame_count += 1
+            else:
+                closed_eyes_frame_count = 0
+                drowsy = False
+
+            if closed_eyes_frame_count >= CLOSED_EYES_FRAME_THRESHOLD:
+                drowsy = True
             
+            # 4단계: 결과 표시
+            print(drowsy)
+            if drowsy:
+                cv2.putText(frame, "졸음 감지! 주의하세요!", (30, 60),
+                            cv2.FONT_HERSHEY_SIMPLEX, 1.2, (0, 0, 255), 3)
 
         #     # 4단계: 졸음 판단
 
@@ -75,7 +89,6 @@ def main():
             break
 
             
-
     cap.release()
 
     cv2.destroyAllWindows()
