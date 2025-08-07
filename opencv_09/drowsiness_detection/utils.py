55771898d31c841697550ab6543014cf00b3ab76
Hoyeon Park <133315262+hoyeondev@users.noreply.github.com>
2025-08-07 17:55:27 +0900

    utils : EAR 함수 계산 수정

--- opencv_09/drowsiness_detection/utils.py
+++ opencv_09/drowsiness_detection/utils.py
@@ -1,25 +1,24 @@
-import numpy as np
+from scipy.spatial import distance as dist
 
-def calculate_ear_from_landmarks(landmarks, eye_indices):
+
+def calculate_ear_from_landmarks(eye):
     """
     눈의 EAR 계산
     :param landmarks: 얼굴 전체 68개 랜드마크 좌표
     :param eye_indices: 왼쪽 또는 오른쪽 눈의 인덱스 (6개)
     :return: EAR 값
     """
-    # landmarks: [[(x0, y0), ..., (x67, y67)]] 형태라면 먼저 꺼낸다
-    if isinstance(landmarks[0], tuple):  # [(x0, y0), ...] 형태인 경우
-        pts = landmarks
-    else:
-        pts = landmarks[0]  # [[(x, y), ...]] 형태면 한 단계 풀기
+    # compute the euclidean distances between the two sets of
+    # vertical eye landmarks (x, y)-coordinates
+    A = dist.euclidean(eye[1], eye[5])
+    B = dist.euclidean(eye[2], eye[4])
 
-    # 필요한 눈 좌표 추출
-    eye = [pts[i] for i in eye_indices]
+    # compute the euclidean distance between the horizontal
+    # eye landmark (x, y)-coordinates
+    C = dist.euclidean(eye[0], eye[3])
 
-    # EAR 계산 공식 적용
-    A = np.linalg.norm(np.array(eye[1]) - np.array(eye[5]))  # |p2 - p6|
-    B = np.linalg.norm(np.array(eye[2]) - np.array(eye[4]))  # |p3 - p5|
-    C = np.linalg.norm(np.array(eye[0]) - np.array(eye[3]))  # |p1 - p4|
-
+    # compute the eye aspect ratio
     ear = (A + B) / (2.0 * C)
+
+    # return the eye aspect ratio
     return ear
