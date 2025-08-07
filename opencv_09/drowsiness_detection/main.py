a56051fb0d1e1a5d8f8868581c7ebc337891251b
Hoyeon Park <133315262+hoyeondev@users.noreply.github.com>
2025-08-07 17:09:07 +0900

    얼굴 검출 함수 불러오기

--- opencv_09/drowsiness_detection/main.py
+++ opencv_09/drowsiness_detection/main.py
@@ -1,5 +1,7 @@
 import cv2
 import dlib
+from basic_landmark import detect_faces
+
 
 def main():
 
@@ -7,11 +9,9 @@ def main():
 
     detector = dlib.get_frontal_face_detector()
 
-    predictor = dlib.shape_predictor('./models/shape_predictor_68_face_landmarks.dat')
+    predictor = dlib.shape_predictor('../src/shape_predictor_68_face_landmarks.dat')
 
     cap = cv2.VideoCapture(0)
-
-    
 
     while cap.isOpened():
 
@@ -21,41 +21,43 @@ def main():
 
             break
 
-            
+        frame = cv2.flip(frame, 1) # 카메라 좌우반전
 
         # 1단계: 얼굴 검출
 
-        faces = detect_faces(frame, detector)
+        # faces = detect_faces(frame, detector)
+        result_img = detect_faces(frame, detector, predictor, draw_landmarks=True)
+
+        cv2.imshow('Drowsiness Detection', result_img)
+        
+
+        # if len(faces) > 0:
+
+        #     # 2단계: 랜드마크 추출
+
+        #     landmarks = get_landmarks(frame, faces[0], predictor)
+
+            
+
+        #     # 3단계: EAR 계산
+
+        #     ear_value = calculate_ear_from_landmarks(landmarks)
+
+            
+
+        #     # 4단계: 졸음 판단
+
+        #     drowsy_state = check_drowsiness(ear_value)
+
+            
+
+        #     # 5단계: 결과 표시
+
+        #     draw_results(frame, landmarks, ear_value, drowsy_state)
 
         
 
-        if len(faces) > 0:
-
-            # 2단계: 랜드마크 추출
-
-            landmarks = get_landmarks(frame, faces[0], predictor)
-
-            
-
-            # 3단계: EAR 계산
-
-            ear_value = calculate_ear_from_landmarks(landmarks)
-
-            
-
-            # 4단계: 졸음 판단
-
-            drowsy_state = check_drowsiness(ear_value)
-
-            
-
-            # 5단계: 결과 표시
-
-            draw_results(frame, landmarks, ear_value, drowsy_state)
-
-        
-
-        cv2.imshow('Drowsiness Detection', frame)
+        # cv2.imshow('Drowsiness Detection', frame)
 
         if cv2.waitKey(1) == 27:  # ESC 키
 
